60d535397dc8f0f0eef15c97c268fe8c0faf75bb
Namnaem <yositasatiman@gmail.com>
2023-12-22 15:13:15 +0700

    lab10

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	cout << "-------------------- BOOM --------------------- \n" ;
+	while (getline(source, textline)){
+	cout << textline << "\n";
+	}
+	getline(source, textline);
+	cout << "-------------------- HA!! ---------------------" << textline<< "\n";
 
     source.close();
     dest.close();
